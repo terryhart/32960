8760d6e024a6bc7dc67f8123b249d788e46cc5d9
zzswang <zzswang@gmail.com>
2018-11-03 20:04:36 +0800

    fix: send after connected

--- src/tcpcopy.js
+++ src/tcpcopy.js
@@ -4,29 +4,33 @@ import logger from "./logger";
 import { DUP_HOST, DUP_PORT } from "./config";
 
 const client = new net.Socket();
+let connected = false;
 
 function connectToDup() {
   client.connect(
     DUP_PORT,
     DUP_HOST,
     function() {
+      connected = true;
       logger.info("connected to dup host");
     }
   );
 }
 
 client.on("close", function() {
+  connected = false;
   logger.info("connection closed and will reconnect after 5 minutes");
   setTimeout(connectToDup, 5 * 60 * 1000);
 });
 
 client.on("error", function(err) {
+  connected = false;
   logger.info("dup socket error");
   logger.error(err);
 });
 
 export default (ctx, next) => {
-  client.write(ctx.data);
+  if (connected) client.write(ctx.data);
   return next();
 };
 
