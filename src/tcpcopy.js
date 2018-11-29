fbb7a1e4db7054aba95306fafc520a60a28522ac
zzswang <zzswang@gmail.com>
2018-11-29 15:18:01 +0800

    fix: log dup

--- src/tcpcopy.js
+++ src/tcpcopy.js
@@ -3,6 +3,12 @@ import logger from "./logger";
 
 import { DUP_HOST, DUP_PORT } from "./config";
 
+function hexify(buf) {
+  if (buf && buf instanceof Buffer) {
+    return buf.toString("hex");
+  }
+}
+
 const client = new net.Socket();
 let connected = false;
 
@@ -31,6 +37,7 @@ client.on("error", function(err) {
 
 export default (ctx, next) => {
   if (connected) client.write(ctx.data);
+  console.log("dup: ", hexify(ctx.data));
   return next();
 };
 
